83af596ee52f55866a0830cdc4b13178bc4057cd
Daniel Hernández <djhrcode@gmail.com>
2021-09-29 23:36:37 -0500

    NEW: Support for aliases with Vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import VuePlugin from "@vitejs/plugin-VuePlugin";
+import { ViteAliases } from "vite-aliases";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+    plugins: [VuePlugin(), ViteAliases()],
+});
