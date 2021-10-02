c71d708dab6a226957109d79cd3e4734691abeaf
Daniel Hernández <djhrcode@gmail.com>
2021-10-01 21:33:49 -0500

    NEW: Define application routes

--- client/src/plugins/router.js
+++ client/src/plugins/router.js
@@ -2,5 +2,25 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 export const router = createRouter({ 
     history: createWebHistory(),
-    routes: [],
+    routes: [
+        {
+            path: "/",
+            redirect: "/welcome",
+        },
+        {
+            name: "welcome",
+            path: "/welcome",
+            component: () => import("@/pages/welcome/index.vue")   
+        },
+        {
+            name: "progress",
+            path: "/progress",
+            component: () => import("@/pages/progress/index.vue")   
+        },
+        {
+            name: "explore",
+            path: "/explore",
+            component: () => import("@/pages/explore/index.vue")   
+        },
+    ],
 })
