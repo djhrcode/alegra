e4b57bf8ace86dfb99a97d05810522f24409bcfb
Daniel Hernández <djhrcode@gmail.com>
2021-10-09 00:56:36 -0500

    NEW: Loading status page

---
+++ resources/js/pages/_status/loading.vue
@@ -0,0 +1,25 @@
+<template>
+    <div
+        class="
+            is-fullwidth
+            is-fullheight
+            is-flex
+            is-justify-content-center
+            is-align-items-center
+        "
+    >
+        <progress
+            class="progress is-small is-primary"
+            max="100"
+            style="height: 4px; max-height: 4px"
+        >
+            15%
+        </progress>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
